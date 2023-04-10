b54c927bb2122fff19d60d04b11f43d8535ba8e8
zamora1221 <72171173+zamora1221@users.noreply.github.com>
2023-04-10 18:54:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import pytesseract
 import pyautogui
 import re
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
+from datetime import datetime
+import os
+import time
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -15,32 +19,33 @@ class App:
         self.root.title("Text Detection App")
 
         # Set the coordinates and size of the region to capture
-        self.x, self.y, self.width, self.height = 360, 600, 250, 50
+        self.x, self.y, self.width, self.height = 360, 560, 250, 100
         #360, 560, 250, 100
         # Flags to indicate whether the mouse has already been moved for each trade type
         self.spy_trade_opened = False
         self.spy_trade_closed = False
         self.spx_trade_opened = False
         self.spx_trade_closed = False
+        self.opened_strike_price = 0;
 
         self.total_trades = 0
 
         # Create label for displaying screen capture
-        self.img_label = tk.Label(self.root)
+        self.img_label = ttk.Label(self.root)
         self.img_label.pack()
 
         # Create text widget for displaying OCR text
         self.text_widget = tk.Text(self.root, width=50, height=5)
         self.text_widget.pack()
 
-        self.detected_label = tk.Label(self.root, text="", font=("Helvetica", 16))
+        self.detected_label = ttk.Label(self.root, text="", font=("Helvetica", 16))
         self.detected_label.pack()
 
-        self.total_trades_label = tk.Label(self.root, text=f"Total Trades: {self.total_trades}")
+        self.total_trades_label = ttk.Label(self.root, text=f"Total Trades: {self.total_trades}")
         self.total_trades_label.pack()
 
         # Create button for starting and stopping detection
-        self.detect_button = tk.Button(self.root, text="Start Detection", command=self.toggle_detection)
+        self.detect_button = ttk.Button(self.root, text="Start Detection", command=self.toggle_detection)
         self.detect_button.pack()
 
         self.detecting = False
@@ -55,6 +60,9 @@ class App:
             self.detect_text()
 
     def detect_text(self):
+        hour = 14
+        minute = 45
+
         while self.detecting:
             # Capture the region of the screen
             img = pyautogui.screenshot(region=(self.x, self.y, self.width, self.height))
@@ -70,20 +78,23 @@ class App:
 
             # Check if the text "BTO SPY XXXc" is present
             if re.search(r'.*[Bb][Tt][Oo].*[Ss][Pp][Yy].*\d+[Cc¢]', text):
+                strike_price = re.search(r'.*[Bb][Tt][Oo].*[Ss][Pp][Yy].*(\d+)[Cc¢]', text).group(1)
                 # Open a new SPY trade if one hasn't already been opened
                 if not self.spy_trade_opened and not self.spx_trade_opened:
                     self.text_widget.insert("end", "Trade opened: BTO SPY XXXc\n")
-                    pyautogui.moveTo(1730, 100)
+                    pyautogui.moveTo(1780, 200)
                     pyautogui.click()
                     self.spy_trade_opened = True
                     self.spy_trade_closed = False
+                    self.opened_strike_price = strike_price
 
             # Check if the text "STC SPY XXXc" is present
-            if re.search(r'[Ss][Tt][Cc].*[Ss][Pp][Yy].*\d+[Cc¢]', text):
+            if re.search(r'[Ss][Tt][Cce].*[Ss][Pp][Yy].*\d+[Cc¢]', text):
+                strike_price = re.search(r'[Ss][Tt][Cce].*[Ss][Pp][Yy].*(\d+)[Cc¢]', text).group(1)
                 # Close the current SPY trade if one has been opened but not yet closed
-                if self.spy_trade_opened and not self.spy_trade_closed:
+                if self.spy_trade_opened and not self.spy_trade_closed and self.opened_strike_price == strike_price:
                     self.text_widget.insert("end", "Trade closed: STC SPY XXXc\n")
-                    pyautogui.moveTo(1800, 100)
+                    pyautogui.moveTo(1700, 200)
                     pyautogui.click()
                     self.spy_trade_closed = True
                     self.spy_trade_opened = False
@@ -91,20 +102,23 @@ class App:
                     self.total_trades_label.config(text=f"Total Trades: {self.total_trades}")
 
             if re.search(r'[Bb][Tt][Oo].*[Ss][Pp][Yy].*\d+[Pp]', text):
+                strike_price = re.search(r'.*[Bb][Tt][Oo].*[Ss][Pp][Yy].*(\d+)[Pp]', text).group(1)
                 # Open a new SPY trade if one hasn't already been opened
                 if not self.spy_trade_opened and not self.spx_trade_opened:
                     self.text_widget.insert("end", "Trade opened: BTO SPY XXXp\n")
-                    pyautogui.moveTo(1730, 285)
+                    pyautogui.moveTo(1780, 200)
                     pyautogui.click()
                     self.spy_trade_opened = True
                     self.spy_trade_closed = False
+                    self.opened_strike_price = strike_price
 
             # Check if the text "STC SPY XXXc" is present
-            if re.search(r'[Ss][Tt][Cc].*[Ss][Pp][Yy].*\d+[Pp]', text):
+            if re.search(r'[Ss][Tt][Cce].*[Ss][Pp][Yy].*\d+[Pp]', text):
+                strike_price = re.search(r'.*[Ss][Tt][Cce].*[Ss][Pp][Yy].*(\d+)[Pp]', text).group(1)
                 # Close the current SPY trade if one has been opened but not yet closed
-                if self.spy_trade_opened and not self.spy_trade_closed:
+                if self.spy_trade_opened and not self.spy_trade_closed and self.opened_strike_price == strike_price:
                     self.text_widget.insert("end", "Trade closed: STC SPY XXXp\n")
-                    pyautogui.moveTo(1800, 285)
+                    pyautogui.moveTo(1700, 200)
                     pyautogui.click()
                     self.spy_trade_closed = True
                     self.spy_trade_opened = False
@@ -112,20 +126,23 @@ class App:
                     self.total_trades_label.config(text=f"Total Trades: {self.total_trades}")
 
             if re.search(r'[Bb][Tt][Oo].*[Ss][Pp][Xx].*\d+[Cc¢]', text):
+                strike_price = re.search(r'.*[Bb][Tt][Oo].*[Ss][Pp][Xx].*(\d+)[Cc¢]', text).group(1)
                 # Open a new SPY trade if one hasn't already been opened
                 if not self.spx_trade_opened and not self.spy_trade_opened:
                     self.text_widget.insert("end", "Trade opened: BTO SPX XXXc\n")
-                    pyautogui.moveTo(1730, 100)
+                    pyautogui.moveTo(1780, 200)
                     pyautogui.click()
                     self.spx_trade_opened = True
                     self.spx_trade_closed = False
+                    self.opened_strike_price = strike_price
 
             # Check if the text "STC SPY XXXc" is present
-            if re.search(r'[Ss][Tt][Cc].*[Ss][Pp][Xx].*\d+[Cc¢]', text):
+            if re.search(r'[Ss][Tt][Cce].*[Ss][Pp][Xx].*\d+[Cc¢]', text):
+                strike_price = re.search(r'.*[Ss][Tt][Cce].*[Ss][Pp][Xx].*(\d+)[Cc¢]', text).group(1)
                 # Close the current SPY trade if one has been opened but not yet closed
-                if self.spx_trade_opened and not self.spx_trade_closed:
+                if self.spx_trade_opened and not self.spx_trade_closed and self.opened_strike_price == strike_price:
                     self.text_widget.insert("end", "Trade closed: STC SPX XXXc\n")
-                    pyautogui.moveTo(1800, 100)
+                    pyautogui.moveTo(1700, 200)
                     pyautogui.click()
                     self.spx_trade_closed = True
                     self.spx_trade_opened = False
@@ -133,25 +150,36 @@ class App:
                     self.total_trades_label.config(text=f"Total Trades: {self.total_trades}")
 
             if re.search(r'[Bb][Tt][Oo].*[Ss][Pp][Xx].*\d+[Pp]', text):
+                strike_price = re.search(r'.*[Bb][Tt][Oo].*[Ss][Pp][Xx].*(\d+)[Pp]', text).group(1)
                 # Open a new SPY trade if one hasn't already been opened
-                if not self.spx_trade_opened and not self.spy_trade_opened:
+                if not self.spy_trade_opened and not self.spx_trade_opened:
                     self.text_widget.insert("end", "Trade opened: BTO SPX XXXp\n")
-                    pyautogui.moveTo(1730, 285)
+                    pyautogui.moveTo(1780, 200)
                     pyautogui.click()
                     self.spx_trade_opened = True
                     self.spx_trade_closed = False
+                    self.opened_strike_price = strike_price
 
             # Check if the text "STC SPY XXXc" is present
-            if re.search(r'[Ss][Tt][Cc].*[Ss][Pp][Xx].*\d+[Pp]', text):
+            if re.search(r'[Ss][Tt][Cce].*[Ss][Pp][Xx].*\d+[Pp]', text):
+                strike_price = re.search(r'.*[Ss][Tt][Cce].*[Ss][Pp][Xx].*(\d+)[Pp]', text).group(1)
                 # Close the current SPY trade if one has been opened but not yet closed
-                if self.spx_trade_opened and not self.spx_trade_closed:
+                if self.spx_trade_opened and not self.spx_trade_closed and self.opened_strike_price == strike_price:
                     self.text_widget.insert("end", "Trade closed: STC SPX XXXp\n")
-                    pyautogui.moveTo(1800, 285)
+                    pyautogui.moveTo(1700, 200)
                     pyautogui.click()
                     self.spx_trade_closed = True
                     self.spx_trade_opened = False
                     self.total_trades += 1
                     self.total_trades_label.config(text=f"Total Trades: {self.total_trades}")
+
+            current_time = time.localtime()
+
+            # Check if the current time matches the sleep time
+            if current_time.tm_hour == hour and current_time.tm_min == minute:
+                # Put the computer into sleep mode
+                os.system("rundll32.exe powrprof.dll,SetSuspendState Standby")
+                break
 
             # Update the image label with the latest screenshot
             img = Image.fromarray(np.array(img))
@@ -159,6 +187,8 @@ class App:
             self.img_label.imgtk = imgtk
             self.img_label.configure(image=imgtk)
             self.root.update()
+
+
 
 
 def main():
